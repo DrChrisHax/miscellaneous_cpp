d37dfe410c26deeef0feaa741dcc7ee93c810508
DrChrisHax <cmanlove1234@outlook.com>
2026-01-03 14:36:34 -0800

    Clean up

--- app/main.cpp
+++ app/main.cpp
@@ -10,19 +10,15 @@ int main(int argc, char* argv[]) {
 
     (void)argc;
     (void)argv;
-
     core::Timer t;
 
+    // Temp testing code
+
+
+    
+
+
     const double ts = t.elapsed();
-
-    Fraction f1(1, 4);
-    Fraction f2(3, 4);
-
-    std::cout << f1 - f2 << "\n";
-
-    std::cout << "Time taken: " << std::to_string(ts);
-
-    std::cout << std::endl;
-
+    std::cout << "Time taken: " << std::to_string(ts) << std::endl;
 	return 0;
 }
