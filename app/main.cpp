c91e09f673f0c197147856f27f5c884301c5d335
DrChrisHax <cmanlove1234@outlook.com>
2026-01-09 14:35:31 -0800

    Added the random header

--- app/main.cpp
+++ app/main.cpp
@@ -4,51 +4,24 @@
 #include "string_helpers.hpp"
 #include "sorts.hpp"
 #include "Timer.h"
+#include "concepts.hpp"
+#include "random.hpp"
 
 #include <iostream>
-#include <iomanip>
-
-#include <array>
-#include <algorithm>
-#include <string>
-#include <string_view>
-#include <functional>
-#include <map>
-
-#include <iterator>
-
-
-
 
 int main(int argc, char* argv[]) {
-
     (void)argc;
     (void)argv;
     core::Timer t;
     // Start testing code
 
-    Fraction<int> f1(99, 872'415'232); // 3*3*11 / 2^26 * 13
-    Fraction<int> f2(51, 1'140'850'688); // 17*3 / 2^26 * 17
-    Fraction<int> expected(69, 436'207'616); // 3*23 / 2^25 * 13
-
-    std::cout << std::setprecision(32);
-    std::cout << "f1: " << f1 << "\n";
-    std::cout << "f2: " << f2 << "\n";
-    std::cout << "expected: " << expected << "\n";
-
-    std::cout << "The larger of f1 and f2 is: " << max(f1, f2) << "\n";
-
-    Fraction<int> result;
-    try {
-        result = f1 + f2;
-    } catch (const std::overflow_error& e) {
-        std::cout << e.what() << std::endl;
-    }
-
-    std::cout << "result: " << result << "\n";
+    std::cout << Random::get<std::size_t>(1, 6) << "\n"; // returns std::size_t
+	std::cout << Random::get<std::size_t>(1, 6u) << "\n"; // returns std::size_t
+    std::cout << Random::get<int>(1, 6u) << "\n"; // returns int
 
     // End testing code
     const double ts = t.elapsed();
     std::cout << "\nTime taken: " << std::to_string(ts) << std::endl;
 	return 0;
 }
+
